edda6fc9f95b770ab74f84a57f037776d1f62bef
Robert MacLean <robert@sadev.co.za>
2017-08-29 20:52:48 +0200

    More tweaking and setting the dates for CFS

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
-var path = require('path');
 
 gulp.task('style', function() {
-    return gulp.src('./public/css/_less/style.less')
-        .pipe(less({
-            paths: ['./node_modules/bootstrap-less/bootstrap']
-        }))
-        .pipe(gulp.dest('./public/css'));
+  return gulp
+    .src('./public/css/_less/style.less')
+    .pipe(
+      less({
+        paths: ['./node_modules/bootstrap-less/bootstrap'],
+      })
+    ).pipe(gulp.dest('./public/css'));
 });
 
-gulp.task('scripts', function() {
-	return gulp.src('./node_modules/jquery/dist/jquery.min.js')
-		.pipe(gulp.dest('./public/scripts'));
+gulp.task('update', done => {
+  gulp.src('./node_modules/bootstrap-less/js/bootstrap.min.js').pipe(gulp.dest('./public/scripts'));
+  done();
 });
