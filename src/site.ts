fcc62a7bdc05245606d67539a4bd912745fc469b
Robert MacLean <robert@sadev.co.za>
2019-10-15 00:05:36 +0200

    Added all the popup and bio functionality

--- src/site.ts
+++ src/site.ts
@@ -32,6 +32,8 @@ if (agendaPlaceholder) {
 }
 
 function loadEventSessions(id: String, target: HTMLElement) {
+    let eventData: Sessionize.Event;
+
     const getTemplate = (id: string) => {
         return ((document.getElementById(id) as HTMLTemplateElement)
             .cloneNode(true) as HTMLTemplateElement)
@@ -50,11 +52,119 @@ function loadEventSessions(id: String, target: HTMLElement) {
         });
     }
 
+    const getSpeakerInfo = (sessionSpeakers: Array<string>) => {
+        return eventData.speakers.filter(s => sessionSpeakers.indexOf(s.id) >= 0);
+    }
+
+    const multipleSpeakerNames = (sessionSpeakers: Array<string>): string => {
+        const remappedSpeakers = getSpeakerInfo(sessionSpeakers).map((s) => s.fullName);
+
+        if (remappedSpeakers.length === 1) {
+            return remappedSpeakers[0];
+        }
+
+        return remappedSpeakers.filter((value, index) => index < remappedSpeakers.length - 1).join(', ') + '& ' + remappedSpeakers[remappedSpeakers.length - 1];
+    }
+
+    const singleSpeaker = (sessionSpeakers: Array<string>) => {
+        return getSpeakerInfo(sessionSpeakers)[0];
+    };
+
+    const singleSpeakerImage = (sessionSpeakers: Array<string>): string => {
+        return singleSpeaker(sessionSpeakers).profilePicture;
+    };
+
+    const closePopup = () => {
+        document.querySelector("div.popupBackdrop")!.classList.add("popupBackdropHidden");
+        document.removeEventListener("keydown", handleCloseKeyPress);
+    }
+
+    const handleCloseKeyPress = (event: KeyboardEvent) => {
+        if (event.key === "Escape") {
+            closePopup();
+        }
+    }
+
+    const buildSocialBadges = (speaker: Sessionize.Speaker) => {
+        const result: Array<HTMLAnchorElement> = [];
+        speaker.links.forEach(link => {
+            const aTag = document.createElement("a");
+            aTag.target = "_blank";
+            aTag.href = link.url;
+            const image = document.createElement("img");
+            image.alt = link.title;
+            switch (link.title) {
+                case "Twitter": {
+                    break;
+                }
+                case "LinkedIn": {
+                    break;
+                }
+            }
+            aTag.appendChild(image);
+            result.push(aTag);
+        });
+
+        return result;
+    }
+
+    const addPopupHandler = () => {
+        const popupClose = document.querySelector("div.popupClose")! as HTMLDivElement;
+        popupClose.onclick = (e) => {
+            closePopup();
+        };
+
+        const backdrop = document.querySelector("div.popupBackdropHidden")!;
+        backdrop.addEventListener("click", closePopup);
+
+        document.querySelectorAll('.agenda-session').forEach((element) => {
+            const div = element as HTMLDivElement;
+            const id = element.attributes["data-slot-id"].value;
+            if (id && id !== "0") {
+                div.classList.add("clickable-session");
+                div.onclick = (e) => {
+                    const id = div.attributes["data-slot-id"].value;
+                    const matchedSession = getSession(id);
+                    backdrop.classList.remove("popupBackdropHidden");
+
+                    document.addEventListener("keydown", handleCloseKeyPress);
+
+                    const popupContent = document.querySelector("div.popupContent")!;
+                    popupContent.querySelectorAll("div.popupBio").forEach((value) => {
+                        popupContent.removeChild(value);
+                    });
+
+                    const speakerInfo = singleSpeaker(matchedSession.speakers);
+                    const socialLinks = buildSocialBadges(speakerInfo);
+                    const bioContent = getTemplate("popupBioContent").firstElementChild!;
+                    (bioContent.querySelector("img.largePopupImage")! as HTMLImageElement).src = speakerInfo.profilePicture;
+                    (bioContent.querySelector("div.bio-speaker")! as HTMLDivElement).innerText = multipleSpeakerNames(matchedSession.speakers);
+                    const socialLinkPlaceholder = (bioContent.querySelector("div.bio-social")! as HTMLDivElement);
+                    socialLinks.forEach(link => {
+                        socialLinkPlaceholder.appendChild(link);
+                    });
+
+                    (bioContent.querySelector("div.bio-tagline")! as HTMLDivElement).innerText = speakerInfo.tagLine;
+                    (bioContent.querySelector("div.bio-title")! as HTMLDivElement).innerText = matchedSession.title;
+                    (bioContent.querySelector("div.bio-talk-description")! as HTMLDivElement).innerText = matchedSession.description;
+                    (bioContent.querySelector("div.bio-speaker-bio")! as HTMLDivElement).innerText = speakerInfo.bio;
+                    popupContent.insertAdjacentElement("beforeend", bioContent);
+                };
+            }
+        });
+    }
+
+    const getSession = (sessionId: String) => {
+        return eventData.sessions.filter(session => session.id === sessionId)[0];
+    }
+
     const parseEventData = (event: Sessionize.Event) => {
+        eventData = event;
+
         document.querySelectorAll(".agenda-session").forEach((element, index) => {
             const div = element as HTMLDivElement;
             const id = div.attributes["data-slot-id"].value;
-            const matchedSession = event.sessions.filter(session => session.id === id)[0];
+            const matchedSession = getSession(id);
 
             if (!matchedSession) {
                 const tbaTemplate = getTemplate("tbaCardTemplate").querySelector("div");
@@ -62,12 +172,31 @@ function loadEventSessions(id: String, target: HTMLElement) {
             } else {
                 const templateDivs = getTemplate("sessionCardTemplate").querySelectorAll("div");
                 templateDivs.forEach((templateElement) => {
+                    switch (templateElement.className) {
+                        case "agenda-session-image": {
+                            const imageElement = document.createElement("img");
+                            imageElement.src = singleSpeakerImage(matchedSession.speakers);
+                            imageElement.classList.add("speaker-image");
+                            templateElement.appendChild(imageElement);
+                            break;
+                        }
+                        case "agenda-session-name": {
+                            templateElement.innerText = multipleSpeakerNames(matchedSession.speakers);
+                            break;
+                        }
+                        case "agenda-session-title": {
+                            templateElement.innerText = matchedSession.title;
+                            break;
+                        }
+                    }
+
                     div.insertAdjacentElement("beforeend", templateElement);
                 })
             }
         });
 
-        toggleUI();    
+        addPopupHandler();
+        toggleUI();
     }
 
     const loadStoredData = () => {
